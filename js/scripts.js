ad09b777954f486f5fc10d9686396991cf5e9b93
Antoaneta-Ivanova <25IMC10124@fh-krems.ac.at>
2025-12-12 18:29:57 +0100

    buyer info

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*Cart*/
+/*Add to Cart Buttons*/
 
 let cart = []
 
@@ -7,15 +7,11 @@ let buttons = document.querySelectorAll(".addtocartbtn");
 function addtoCart(event){
 
     let clickedBtn = event.target;
-
     cart.push({name: clickedBtn.dataset.name, price: Number(clickedBtn.dataset.price)});
-
     document.getElementById("cartcount").innerText = cart.length;
-
     console.log(cart);
 
     let total = 0;
-
     for (let item of cart) {
     total += item.price;
     }
@@ -25,3 +21,32 @@ function addtoCart(event){
 for (let button of buttons){
     button.addEventListener("click", addtoCart)
 }
+
+
+/*Reset Cart Button*/
+
+let resetbutton = document.querySelector("#resetbtn")
+
+function resetCart() {
+    cart = [];
+    document.getElementById("cartcount").innerText = cart.length;
+    document.getElementById("totalprice").innerText = 0;
+    console.log(cart)
+}
+
+resetbutton.addEventListener("click", resetCart)
+
+
+/*Check Out Button */
+
+let checkoutbutton = document.getElementById("checkoutbtn")
+let information = document.getElementById("intro")
+
+function checkOut(){
+    document.getElementById("shoppingsection").style.display = "none";
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("buyerinformation").classList.remove("hide");
+
+}
+
+checkoutbutton.addEventListener("click", checkOut)
