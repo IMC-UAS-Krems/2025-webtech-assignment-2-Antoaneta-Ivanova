5b400173ea30ee2c72fa73f640dcf1eecf20e507
Antoaneta-Ivanova <25IMC10124@fh-krems.ac.at>
2025-12-13 15:55:25 +0100

    totalcost

--- js/scripts.js
+++ js/scripts.js
@@ -103,8 +103,36 @@ function summaryCart(){
         point.innerText = `${item.name}: €${item.price}`;
         listsummary.appendChild(point)
     }
+    document.getElementById("subtotal").innerText = calculateSubtotal()
+    document.getElementById("discount").innerText = calculateDiscount()
+    document.getElementById("totalcost").innerText = calculatetotalCost()
 }
 
+function calculateSubtotal(){
+    let total = 0;
+    for (let item of cart) {
+    total += item.price;
+    }
+    return total
+}
+
+function calculateDiscount(){
+
+    let subtotal = calculateSubtotal();
+    let discount = 0;
+
+    if (cart.length >= 3) {
+        discount = 0.1 * subtotal;
+    }
+    return discount;
+}
+
+function calculatetotalCost(){
+    let subtotal = calculateSubtotal();
+    let discount = calculateDiscount();
+    let totalcost = subtotal - discount;
+    return totalcost
+}
 
 
 
